00a00482409dd853e37954c4b8937b75391eb0c1
jinsDevelopment <0_0lllllll@naver.com>
2022-05-09 22:56:00 +0900

    [refactor] Google 로그인 리팩토링

--- src/main/java/com/hanghae/coffee/dto/oauthProperties/GoogleUserInfoDto.java
+++ src/main/java/com/hanghae/coffee/dto/oauthProperties/GoogleUserInfoDto.java
@@ -1,14 +1,38 @@
 package com.hanghae.coffee.dto.oauthProperties;
 
+import com.hanghae.coffee.model.OauthType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
-@AllArgsConstructor
-public class GoogleUserInfoDto {
-    private String id;
-    private String name;
-    private String email;
+public class GoogleUserInfoDto extends UserInfoDto{
+
+    public GoogleUserInfoDto(String authId, String nickname, String email, String profileUrl) {
+        super(authId, nickname, email, profileUrl);
+    }
+
+    @Override
+    public String getAuthId() {
+        return authId;
+    }
+
+    @Override
+    public String getNickname() {
+        return nickname;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    @Override
+    public OauthType getOauthType() {
+        return OauthType.GOOGLE;
+    }
 }
--- src/main/java/com/hanghae/coffee/service/UserInfoFactory.java
+++ src/main/java/com/hanghae/coffee/service/UserInfoFactory.java
@@ -1,6 +1,7 @@
 package com.hanghae.coffee.service;
 
 import com.hanghae.coffee.advice.RestException;
+import com.hanghae.coffee.dto.oauthProperties.GoogleUserInfoDto;
 import com.hanghae.coffee.dto.oauthProperties.KakaoUserInfoDto;
 import com.hanghae.coffee.dto.oauthProperties.NaverUserInfoDto;
 import com.hanghae.coffee.dto.oauthProperties.UserInfoDto;
@@ -13,7 +14,7 @@ import org.springframework.http.HttpStatus;
 public class UserInfoFactory {
     public static UserInfoDto getOAuth2UserInfo(OauthType oauthType, String id, String nickname, String email, String profileUrl) {
         switch (oauthType) {
-//            case GOOGLE: return new GoogleOauth2UserInfo(attributes);
+            case GOOGLE: return new GoogleUserInfoDto(id, nickname, email, profileUrl);
             case NAVER: return new NaverUserInfoDto(id, nickname, email, profileUrl);
             case KAKAO: return new KakaoUserInfoDto(id, nickname, email, profileUrl);
             default: throw new RestException(HttpStatus.BAD_REQUEST, "알 수 없는 oAuthType 입니다.");
