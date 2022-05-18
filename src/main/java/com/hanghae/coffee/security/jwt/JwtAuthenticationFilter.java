9ecdb927b0a3fcface196c9a48a79bbc2118cb21
이진서 <101075624+jinsDevelopment@users.noreply.github.com>
2022-05-18 22:38:08 +0900

    [fix] import 수정

--- src/main/java/com/hanghae/coffee/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hanghae/coffee/security/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
