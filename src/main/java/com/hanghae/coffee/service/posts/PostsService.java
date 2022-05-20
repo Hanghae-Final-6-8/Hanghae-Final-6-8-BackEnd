3363e33f08c57b747c1c3f1aed792718779a21e5
devgusehdrh <dev.gusehdrh@gmail.com>
2022-05-21 04:00:29 +0900

    [fix&refactor] 태그 기능 수정, 게시판 수정 기능 수정

--- src/main/java/com/hanghae/coffee/service/posts/PostsService.java
+++ src/main/java/com/hanghae/coffee/service/posts/PostsService.java
@@ -98,12 +98,12 @@ public class PostsService {
 
     public Posts getPosts(Long postId, Long userId) {
         Posts posts = postsRepository.findById(postId).orElseThrow(
-            () -> new RestException(HttpStatus.BAD_REQUEST, "해당 게시물이 없습니다.")
+            () -> new RestException(HttpStatus.BAD_REQUEST, "bad request")
         );
 
         if (!posts.getUsers().getId().equals(userId)) {
 
-            new RestException(HttpStatus.BAD_REQUEST, "유저 정보가 일치하지 않습니다.");
+            throw new RestException(HttpStatus.FORBIDDEN, "forbidden");
 
         }
 
