28c790282d51d97d2cd4c23f6b4e1862d845d054
devgusehdrh <dev.gusehdrh@gmail.com>
2022-05-26 18:52:57 +0900

    [refactor] setter 어노테이션 제거

--- src/main/java/com/hanghae/coffee/model/Tags.java
+++ src/main/java/com/hanghae/coffee/model/Tags.java
@@ -1,21 +1,17 @@
 package com.hanghae.coffee.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Tags {
 
