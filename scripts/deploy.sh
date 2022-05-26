808e49738790007f34f21c7109afe7d72515ec72
devgusehdrh <98457348+devgusehdrh@users.noreply.github.com>
2022-05-27 06:50:48 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,47 +1,22 @@
-PROJECT=final_project
-REPOSITORY=/home/ubuntu/deploy
-LOGS_DIRECTORY=/home/ubuntu/deploy/logs
-LOG_BACKUP_DIRECTORY=/home/ubuntu/deploy/log-backup
+#!/bin/bash
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+PROJECT=final
+REPOSITORY=/home/ubuntu/sparta
 
-CURRENT_PID=$(pgrep -fl $PROJECT | grep java | awk '{print $1}')
+CONTAINER_ID=$(docker container ls -f "name=final" -q)
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 컨테이너 ID : ${CONTAINER_ID}"
 
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z ${CONTAINER_ID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/docker/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 "$CURRENT_PID"
+  echo "> docker stop ${CONTAINER_ID}"
+  sudo docker stop ${CONTAINER_ID}
+  echo "> docker rm ${CONTAINER_ID}"
+  sudo docker rm ${CONTAINER_ID}
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(find $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x "$JAR_NAME"
-
-echo "> $JAR_NAME 실행"
-
-if [ -d $LOGS_DIRECTORY ]
-then
-  echo "$LOGS_DIRECTORY 가 이미 존재합니다."
-else
-  ln -s /opt/codedeploy-agent/logs /home/ubuntu/deploy/logs
-fi
-
-if [ -d $LOG_BACKUP_DIRECTORY ]
-then
-  echo "$LOG_BACKUP_DIRECTORY 가 이미 존재합니다."
-else
-  ln -s /opt/codedeploy-agent/log-backup /home/ubuntu/deploy/log-backup
-fi
-
-# 실행
-nohup java -Duser.timezone=KST -jar "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
+cd /home/ubuntu/final && docker build -t final .
+docker run --name final -d -e active=prod -p 8080:8080 final
