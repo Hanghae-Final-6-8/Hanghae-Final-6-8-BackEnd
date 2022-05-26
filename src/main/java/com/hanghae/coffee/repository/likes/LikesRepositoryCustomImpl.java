6a7622b3e23d2dd052fd4b7c23d3e1c800474d57
devgusehdrh <dev.gusehdrh@gmail.com>
2022-05-26 21:09:32 +0900

    [fix] 내가 누른 좋아요 게시물 목록 출력 query 수정

--- src/main/java/com/hanghae/coffee/repository/likes/LikesRepositoryCustomImpl.java
+++ src/main/java/com/hanghae/coffee/repository/likes/LikesRepositoryCustomImpl.java
@@ -2,14 +2,18 @@ package com.hanghae.coffee.repository.likes;
 
 import static com.hanghae.coffee.model.QLikes.likes;
 import static com.hanghae.coffee.model.QPosts.posts;
+import static com.hanghae.coffee.model.QUsers.users;
 import static com.hanghae.coffee.model.QPostsImage.postsImage;
 import static com.hanghae.coffee.model.QPostsTags.postsTags;
 import static com.hanghae.coffee.model.QTags.tags;
 
-import com.hanghae.coffee.dto.likes.LikesDto;
 import com.hanghae.coffee.dto.posts.PostsDto;
+import com.hanghae.coffee.repository.common.OrderByNull;
 import com.hanghae.coffee.repository.helper.RepositorySliceHelper;
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,23 +30,45 @@ public class LikesRepositoryCustomImpl implements LikesRepositoryCustom {
   public Slice<PostsDto> getAllByOrderByUser_IdwithPosts(Long id, Pageable pageable){
     List<PostsDto> postsDtoList = jpaQueryFactory
         .select(
-            Projections.fields(PostsDto.class,
+            Projections.bean(PostsDto.class,
                 posts.id.as("posts_id"),
                 posts.title,
                 posts.content,
                 posts.users.nickname,
-                posts.createdAt,
-                posts.modifiedAt,
-                postsImage.imageUrl.as("posts_image")
+                posts.createdAt.as("created_at"),
+                posts.modifiedAt.as("modified_at"),
+                postsImage.imageUrl.as("posts_image"),
+                ExpressionUtils.
+                    as(JPAExpressions
+                            .select(likes.posts.id.count())
+                            .from(likes)
+                            .where(likes.posts.id.eq(posts.id))
+                            .groupBy(likes.posts.id)
+                            .orderBy(OrderByNull.DEFAULT),
+                        "likes_count"),
+                ExpressionUtils.
+                    as(JPAExpressions
+                            .select(likes.users.id)
+                            .from(likes)
+                            .where(posts.id.eq(likes.posts.id))
+                            .where(likes.users.id.eq(id)),
+                        "isLikes"),
+                ExpressionUtils.
+                    as(JPAExpressions
+                            .select(Expressions.stringTemplate("group_concat({0})", tags.tagName)
+                            )
+                            .from(postsTags)
+                            .innerJoin(postsTags.tags, tags)
+                            .where(posts.id.eq(postsTags.posts.id))
+                            .groupBy(posts.id)
+                            .orderBy(OrderByNull.DEFAULT),
+                        "tag_name")
             ))
         .from(posts)
-        .leftJoin(likes)
-          .on(posts.id.eq(likes.posts.id))
         .leftJoin(posts.postsImages, postsImage)
-        .leftJoin(posts.postsTags, postsTags)
-        .leftJoin(tags)
-          .on(postsTags.tags.id.eq(tags.id))
-        .where(likes.users.id.eq(id))
+        .innerJoin(posts.users,users)
+        .where(posts.users.id.eq(id))
+        .orderBy(posts.modifiedAt.desc())
         .offset(pageable.getOffset())
         .limit(pageable.getPageSize() + 1)
         .fetch();
